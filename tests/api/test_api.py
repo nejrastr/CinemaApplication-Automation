dee79166db5df18e0b87e1b151a6ef56e15725a2
nejrastr <nstrsevic1@etf.unsa.ba>
2026-01-26 22:37:44 +0100

    Updated test steps

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -3,18 +3,17 @@ from typing import Any
 import allure
 from data.test_data import TEST_USER
 from tasks.api_tasks import ApiTasks
-from data.data import get_api_filters, get_movie_projections
+from data.data import get_api_filters, get_movie_projections, get_movie_details_data
 
 @allure.epic("Cinema Application")
 @allure.feature("Api Smoke Suite")
 class TestApi(ApiTasks):
     db: Any
-    @allure.story("Full end-to-end movie discovery and authentication flow")
+    @allure.story("Full end-to-end movie discovery, authentication and reservation flow")
     @allure.severity(allure.severity_level.CRITICAL)
-    def test_api_flow(self):
+    def test_movie_reservation_api_flow(self):
         with allure.step("Step 1: Test data setup"):
-            api_filters_data = get_api_filters(self.db)
-            api_movie_projections_data = get_movie_projections(self.db)
+            api_filters_data, api_movie_projections_data, api_test_data = self.setup_test_data()
         with allure.step("Step 2: Retrieve currently showing movies"):
             self.get_currently_showing_movies(**api_filters_data["current"])
         with allure.step("Step 2: Explore movie metadata (Cities, Venues, Genres)"):
@@ -24,7 +23,7 @@ class TestApi(ApiTasks):
         with allure.step("Step 4: Filter and verify upcoming movies"):
             self.get_upcoming_movies(**api_filters_data["filters"])
         with allure.step("Step 5: Verify specific movie projections by ID and Date"):
-            self.get_movies_projections_by_movieId(**api_movie_projections_data)
+            self.get_movies_projections_by_movieId(api_movie_projections_data["movie_id"], api_movie_projections_data["projection_date"])
         with allure.step("Step 6: Register and Verify New User via Gmail"):
             user_payload = TEST_USER.copy()
             email_used = self.register_and_verify_user(user_payload)
@@ -32,5 +31,46 @@ class TestApi(ApiTasks):
             self.login(email=email_used, password=TEST_USER["password"])
         with allure.step("Step 8: Validate user profile information"):
             self.get_user_profile(expected_email=email_used)
-        with allure.step("Step 9: Logout and terminate session"):
+        with allure.step("Step 9: Validate movie details information"):
+            self.get_movie_details(api_test_data)
+        with allure.step("Step 10: Reserve movie ticket"):
+            self.get_movie_ticket_reservation(api_test_data)
+        with allure.step("Step 11: Logout and terminate session"):
             self.logout()
+
+    @allure.story("Full end-to-end movie discovery, authentication and ticket purchase flow")
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_movie_payment_api_flow(self):
+        with allure.step("Step 1: Test data setup"):
+            api_filters_data = get_api_filters(self.db)
+            api_movie_projections_data = get_movie_projections(self.db)
+            api_test_data = get_movie_details_data(self.db)
+        with allure.step("Step 2: Retrieve currently showing movies"):
+            self.get_currently_showing_movies(**api_filters_data["current"])
+        with allure.step("Step 2: Explore movie metadata (Cities, Venues, Genres)"):
+            self.get_cities()
+            self.get_venues_by_city(api_filters_data["filters"]["cityId"])
+            self.get_genres()
+        with allure.step("Step 4: Filter and verify upcoming movies"):
+            self.get_upcoming_movies(**api_filters_data["filters"])
+        with allure.step("Step 5: Verify specific movie projections by ID and Date"):
+            self.get_movies_projections_by_movieId(api_movie_projections_data["movie_id"],
+                                                   api_movie_projections_data["projection_date"])
+        with allure.step("Step 6: Register and Verify New User via Gmail"):
+            user_payload = TEST_USER.copy()
+            email_used = self.register_and_verify_user(user_payload)
+        with allure.step("Step 7: Authenticate with registered credentials"):
+            self.login(email=email_used, password=TEST_USER["password"])
+        with allure.step("Step 8: Validate user profile information"):
+            self.get_user_profile(expected_email=email_used)
+        with allure.step("Step 9: Validate movie details information"):
+            self.get_movie_details(api_test_data)
+        # with allure.step("Step 10: Reserve movie ticket"):
+        #     self.get_movie_ticket_reservation(api_test_data)
+        with allure.step("Step 10: Buy movie ticket"):
+            self.buy_movie_tickets(api_test_data)
+        with allure.step("Step 11: Logout and terminate session"):
+            self.logout()
+
+
+
--- tests/test_smoke_ui.py
+++ tests/test_smoke_ui.py
@@ -3,7 +3,7 @@ import allure
 import pytest
 from playwright.sync_api import Page
 from tasks.ui_tasks import Tasks
-from data.data import get_currently_showing_filters, get_upcoming_movies_filters
+from data.data import get_currently_showing_filters, get_upcoming_movies_filters, get_movie_projections, get_movie_details_data
 from data.test_data import TEST_USER
 
 @allure.feature("Smoke Tests")
@@ -11,18 +11,39 @@ from data.test_data import TEST_USER
 class TestSmokeUi:
     page: Page
     db: Any
-    @allure.title("End-to-End Smoke Test: Registration, Login, and Movie Filtering")
+    @allure.title("End-to-End Smoke Test: Registration, Login, Movie Filtering, Ticket reservation")
     @allure.severity(allure.severity_level.CRITICAL)
-    @allure.story("Movie Discovery and Filtering")
-
-    def test_smoke_ui(self):
+    def test_smoke_ui_ticket_reservation(self):
         tasks = Tasks(self.page)
         tasks.complete_registration(TEST_USER)
         tasks.complete_login(TEST_USER)
-        ui_filter_test_data ={
+        ui_filter_test_data = tasks.setup_ui_test_data(self.db)
+        tasks.verify_search_and_filtering_currently_showing(**ui_filter_test_data["currently_showing"])
+        tasks.verify_movie_details(
+            movie_data=ui_filter_test_data["movie_details"],
+            current_showing=ui_filter_test_data["currently_showing"]
+        )
+        tasks.verify_movie_ticket_reservation(ui_filter_test_data["movie_details"]["city"], ui_filter_test_data["movie_details"]["cinema"], ui_filter_test_data["movie_details"]["title"],ui_filter_test_data["movie_details"]["projection_time"], ui_filter_test_data["currently_showing"]["day"], ui_filter_test_data["currently_showing"]["month"], ui_filter_test_data["currently_showing"]["weekday"])
+        tasks.verify_search_and_filtering_upcoming_movies(**ui_filter_test_data["upcoming_movies"])
+        tasks.complete_logout()
+
+    @allure.title("End-to-End Smoke Test: Registration, Login, Movie Filtering, Ticket purchase")
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_smoke_ui_ticket_payment(self):
+        tasks = Tasks(self.page)
+        tasks.complete_registration(TEST_USER)
+        tasks.complete_login(TEST_USER)
+        ui_filter_test_data = {
             "currently_showing": get_currently_showing_filters(self.db),
             "upcoming_movies": get_upcoming_movies_filters(self.db),
+            "movie_projections": get_movie_projections(self.db),
+            "movie_details": get_movie_details_data(self.db),
         }
         tasks.verify_search_and_filtering_currently_showing(**ui_filter_test_data["currently_showing"])
+        tasks.verify_movie_details(**ui_filter_test_data["movie_details"])
+        tasks.verify_movie_ticket_payment(ui_filter_test_data["movie_details"]["city"],
+                                              ui_filter_test_data["movie_details"]["cinema"],
+                                              ui_filter_test_data["movie_details"]["title"],
+                                              ui_filter_test_data["movie_details"]["projection_time"])
         tasks.verify_search_and_filtering_upcoming_movies(**ui_filter_test_data["upcoming_movies"])
         tasks.complete_logout()
